77504ed203df22b7363d96f313586788f9371235
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2014-08-22 16:46:30 +0200

    Contact::preDehydrate

--- Service/Contact.php
+++ Service/Contact.php
@@ -11,4 +11,32 @@ class Contact extends InstanceResource
 	{
 		return self::ENDPOINT_SALES;
 	}
+
+	/** {@inheritdoc} */
+	protected function preDehydrate()
+	{
+		return array_intersect_key($this->data, array_flip([
+			'name',
+			'last_name',
+			'first_name',
+			'contact_id',
+			'email',
+			'phone',
+			'mobile',
+			'twitter',
+			'skype',
+			'facebook',
+			'linkedin',
+			'address',
+			'city',
+			'country',
+			'title',
+			'description',
+			'industry',
+			'website',
+			'fax',
+			'tag_list',
+			'private',
+		]));
+	}
 }
