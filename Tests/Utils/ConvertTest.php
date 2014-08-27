21a674b75be8422e4e6c1f4603c52fc335517265
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2014-08-27 10:49:18 +0200

    ConvertTest

---
+++ Tests/Utils/ConvertTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace prgTW\BaseCRM\Tests\Utils;
+
+use prgTW\BaseCRM\Utils\Convert;
+
+class ConvertTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @dataProvider provideObjectsAndArrays
+	 */
+	public function testObjectToArray($object, array $array)
+	{
+		$converted = Convert::objectToArray($object);
+		$this->assertEquals($array, $converted);
+	}
+
+	public function provideObjectsAndArrays()
+	{
+		$obj1          = new \stdClass();
+		$obj1->a       = 1;
+		$obj1->b       = new \stdClass();
+		$obj1->b->c    = 2;
+		$obj1->b->d    = new \stdClass();
+		$obj1->b->d->e = 3;
+
+		return [
+			[new \stdClass(), []],
+			[$obj1, ['a' => 1, 'b' => ['c' => 2, 'd' => ['e' => 3]]]],
+		];
+	}
+
+
+}
