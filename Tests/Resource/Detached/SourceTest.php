d473f8b0f313e8672e756997e7687f53e0d5ef61
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2014-09-25 10:50:22 +0200

    more tests

--- Tests/Resource/Detached/SourceTest.php
+++ Tests/Resource/Detached/SourceTest.php
@@ -2,6 +2,8 @@
 
 namespace prgTW\BaseCRM\Tests\Detached;
 
+use prgTW\BaseCRM\Resource\CustomField;
+use prgTW\BaseCRM\Resource\CustomFieldsCollection;
 use prgTW\BaseCRM\Service\Detached\Source;
 
 class SourceTest extends \PHPUnit_Framework_TestCase
@@ -19,5 +21,31 @@ class SourceTest extends \PHPUnit_Framework_TestCase
 				'custom1' => 'value1',
 			],
 		], $dehydrated);
+		$this->assertInstanceOf(CustomFieldsCollection::class, $source->getCustomFields());
+		$this->assertInstanceOf(CustomField::class, $source->getCustomField('custom1'));
+		$this->assertNull($source->getCustomField('custom1')->getId());
+		$this->assertEquals('custom1', $source->getCustomField('custom1')->getName());
+		$this->assertEquals('value1', $source->getCustomField('custom1')->getValue());
+		$this->assertEquals(['id' => null, 'value' => 'value1'], $source->getCustomField('custom1')->getData());
+	}
+
+	/**
+	 * @expectedException \PHPUnit_Framework_Error_Warning
+	 */
+	public function testNonExistentField()
+	{
+		$source = new Source();
+		$source->non_existent_field;
+	}
+
+	public function testMagicMethods()
+	{
+		$source     = new Source();
+		$source->id = 1;
+
+		$this->assertEquals(1, $source->id);
+		$this->assertEmpty(false, $source->id);
+		$this->assertFalse(empty($source->id));
+		$this->assertTrue(isset($source->id));
 	}
 }
